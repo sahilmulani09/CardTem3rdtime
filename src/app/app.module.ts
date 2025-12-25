8130a52c8234c0a6cb50d902fd2758ac67137dda
sahilmulani09 <sahilmulani462@gmail.com>
2025-12-26 00:40:42 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, 
+    MatCardModule,           
+    MatButtonModule          
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
